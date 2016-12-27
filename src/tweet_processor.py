3418a624f92142780194ccc11990e93919cb0cc1
Siddharth Srivatsan <sid.srivatsan@gmail.com>
2016-12-27 18:45:07 -0500

    Basics for streaming trump's tweets

---
+++ src/tweet_processor.py
@@ -0,0 +1,51 @@
+import tweepy
+from tweepy import OAuthHandler
+import json
+from tweepy import Stream
+from tweepy.streaming import StreamListener
+from nltk.tokenize import word_tokenize
+import re
+
+emoticons_str = r"""
+    (?:
+        [:=;] # Eyes
+        [oO\-]? # Nose (optional)
+        [D\)\]\(\]/\\OpP] # Mouth
+    )"""
+ 
+regex_str = [
+    emoticons_str,
+    r'<[^>]+>', # HTML tags
+    r'(?:@[\w_]+)', # @-mentions
+    r"(?:\#+[\w_]+[\w\'_\-]*[\w_]+)", # hash-tags
+    r'http[s]?://(?:[a-z]|[0-9]|[$-_@.&amp;+]|[!*\(\),]|(?:%[0-9a-f][0-9a-f]))+', # URLs
+    r'(?:(?:\d+,?)+(?:\.?\d+)?)', # numbers
+    r"(?:[a-z][a-z'\-_]+[a-z])", # words with - and '
+    r'(?:[\w_]+)', # other words
+    r'(?:\S)' # anything else
+]
+    
+tokens_re = re.compile(r'('+'|'.join(regex_str)+')', re.VERBOSE | re.IGNORECASE)
+emoticon_re = re.compile(r'^'+emoticons_str+'$', re.VERBOSE | re.IGNORECASE)
+ 
+def tokenize(s):
+    return tokens_re.findall(s)
+ 
+def preprocess(s, lowercase=False):
+    tokens = tokenize(s)
+    if lowercase:
+        tokens = [token if emoticon_re.search(token) else token.lower() for token in tokens]
+    return tokens
+
+tweet_list = []
+
+f = open('python.json', 'r')
+line = f.readline()
+for line in f:
+	tweet = json.loads(line)
+	tweet_id = tweet['user']['id_str']
+	print(tweet_id)
+	if (tweet_id == '25073877') or (tweet['retweeted'] == 'true'):
+		tweet_text = tweet['text']
+		tweet_list.extend(preprocess(tweet_text))
+print(tweet_list)
